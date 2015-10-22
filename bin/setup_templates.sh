507921396642cfdf3945f3d68bbb814d90641b46
Jason Ziglar <jpz@vt.edu>
2015-10-21 22:36:01 -0400

    Refactoring the scripts to break templates out of the MultiMarkdown support system.

--- bin/setup_templates.sh
+++ bin/setup_templates.sh
@@ -8,7 +8,7 @@ abs_dir="${dir}/.."
 if [ ! -d "${abs_dir}/common" ]; then
   read -p "Please provide a git repository to store templates within:" repo
   if [ -z "$repo" ]; then
-    repo="https://github.com/TRECVT/peg-multimarkdown-latex-support.git"
+    repo="https://github.com/fletcher/peg-multimarkdown-latex-support.git"
   fi
   cur_dir=`pwd`
   cd $abs_dir
@@ -26,6 +26,26 @@ esac
 mkdir -p "${latex_dir}"/tex/latex
 mkdir -p "${latex_dir}"/bibtex
 
-ln -s ${abs_dir}/common "${latex_dir}"/tex/latex/mmd
-ln -s ${abs_dir}/common "${latex_dir}"/bibtex/bst
-ln -s ${abs_dir}/common "${latex_dir}"/bibtex/bib
+mmd_dir="${latex_dir}/tex/latex/mmd"
+bst_dir="${latex_dir}/bibtex/bst"
+bib_dir="${latex_dir}/bibtex/bib"
+
+if [ -d "${latex_dir}"/tex/latex/mmd ]; then
+  echo "Directories for LaTeX already exist, which is problematic. Please determine if the following directories are correct, or remove them and run again."
+  echo $mmd_dir
+  echo $bst_dir
+  echo $bib_dir
+else
+  ln -s ${abs_dir}/common $mmd_dir
+  ln -s ${abs_dir}/common $bst_dir
+  ln -s ${abs_dir}/common $bib_dir
+fi
+
+echo "Looking for dir ${abs_dir}"
+mmdl_dir=`grep "${abs_dir}/templates" ~/.bashrc`
+echo "Done"
+#Environment variable not defined, inject one for this repository
+if [ -z "$mmdl_dir" ]; then
+  echo "Configuring .bashrc"
+  echo "export TEXINPUTS=${abs_dir}/templates/.//:$TEXINPUTS:" >> ~/.bashrc
+fi
