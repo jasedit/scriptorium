73436c5be11be1da2c443b79d134ed41f96bb623
Jason Ziglar <jasedit@gmail.com>
2016-09-15 09:39:49 -0400

    Re-order init to correctly set default arguments during initialization

--- scriptorium/__init__.py
+++ scriptorium/__init__.py
@@ -2,9 +2,10 @@
 
 TEMPLATE_DIR = None
 
-from .papers import paper_root, get_template, to_pdf, create
-from .templates import all_templates, find_template
 from .config import read_config, save_config
-from .__main__ import main
 
 read_config()
+
+from .papers import paper_root, get_template, to_pdf, create
+from .templates import all_templates, find_template
+from .__main__ import main
