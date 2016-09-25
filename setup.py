f6e552ef36b7a4f25546a1ed460723c6adba6e9c
Jason Ziglar <jasedit@gmail.com>
2016-09-25 13:22:33 -0400

    Increment version to 2.2.0

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ long_description = pypandoc.convert_file('README.md', 'rst')
 
 setup(
     name='scriptorium',
-    version='2.1.1',
+    version='2.2.0',
     description='Multimarkdown and LaTeX framework for academic papers.',
     long_description=long_description,
     license='MIT',
