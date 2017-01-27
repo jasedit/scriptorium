8e6d8fb3615a78198d537ad01a2b1603a117e472
Jason Ziglar <jasedit@gmail.com>
2017-01-27 10:06:43 -0500

    Add config .yml extension

--- scriptorium/templates.py
+++ scriptorium/templates.py
@@ -123,7 +123,7 @@ def get_default_config(template, template_dir=None):
     """Get default configuration options if available."""
     template_dir = template_dir if template_dir else scriptorium.TEMPLATE_DIR
     template_loc = find_template(template, template_dir)
-    config_path = os.path.join(template_loc, 'default_config')
+    config_path = os.path.join(template_loc, 'default_config.yml')
     config = {}
 
     if os.path.exists(config_path):
