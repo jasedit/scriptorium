2823b35d3bf3d521ae3c9769e2696455bbed8318
Jason Ziglar <jasedit@gmail.com>
2017-02-20 19:30:49 -0500

    Expand home directory wildcards to ensure path is valid

--- scriptorium/config.py
+++ scriptorium/config.py
@@ -14,12 +14,17 @@ _DEFAULT_CFG = {
     'LATEX_CMD': 'xelatex'
 }
 
+def _sanitize_paths(cfg):
+    """Ensure that paths in configuration options have ~ symbols expanded."""
+    cfg['TEMPLATE_DIR'] = os.path.expanduser(cfg['TEMPLATE_DIR'])
+
 def read_config():
     """Read configuration values for scriptorium."""
     try:
         with open(_CFG_FILE, 'Ur') as cfg_fp:
             cfg = yaml.load(cfg_fp)
             scriptorium.CONFIG.update(cfg)
+            _sanitize_paths(scriptorium.CONFIG)
     except EnvironmentError:
         if not os.path.exists(scriptorium.CONFIG['TEMPLATE_DIR']):
             os.makedirs(scriptorium.CONFIG['TEMPLATE_DIR'])
@@ -28,5 +33,6 @@ def read_config():
 
 def save_config():
     """Save configuration values for scriptorium."""
+    _sanitize_paths(scriptorium.CONFIG)
     with open(_CFG_FILE, 'w') as cfg_fp:
         yaml.dump(scriptorium.CONFIG, cfg_fp)
